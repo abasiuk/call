0ceb65a1b3fe4caecbd76894ae896673a89d4b52
abasiuk <abasiuk8@gmail.com>
2017-12-22 16:13:00 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-	$('.owl-carousel').owlCarousel({
-                loop:false, //Зацикливаем слайдер
+	$('.owl-carousel.office-slider').owlCarousel({
+                loop:true, //Зацикливаем слайдер
                 nav:true, //Вкл навигацию
                 navText: false,
                 autoplay:true, //Автозапуск слайдера
@@ -10,6 +10,22 @@ $(document).ready(function(){
                 mouseDrag: false,
                 touchDrag: false,
                 margin: 20,
-                items: 4
+                items: 4,
+                dots: false,
+                center: true
+    });
+
+    $('.owl-carousel.review-slider').owlCarousel({
+                loop:false, //Зацикливаем слайдер
+                nav:false, //Вкл навигацию
+                navText: false,
+                autoplay:true, //Автозапуск слайдера
+                smartSpeed:3500, //Время движения слайда
+                autoplayTimeout:6000, //Время смены слайда
+                mouseDrag: true,
+                touchDrag: false,
+                margin: 20,
+                dots: false, 
+                items: 3
     });
 });
