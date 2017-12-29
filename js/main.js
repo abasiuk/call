d20036b4be21237f9a330461cf6f1c3326f5ca27
abasiuk <abasiuk8@gmail.com>
2017-12-29 19:10:24 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
+    document.body.onload = function () {
+        setTimeout( function () {
+            var preloader = jQuery('#preloader');
+            if (!preloader.hasClass('done')){
+                preloader.addClass('done');
+            }
+        }, 1000);
+    }
 	$('.owl-carousel.office-slider').owlCarousel({
                 loop:true, //Зацикливаем слайдер
                 nav:true, //Вкл навигацию
@@ -8,11 +15,19 @@ $(document).ready(function(){
                 smartSpeed:3500, //Время движения слайда
                 autoplayTimeout:6000, //Время смены слайда
                 mouseDrag: false,
-                touchDrag: false,
+                touchDrag: true,
                 margin: 20,
                 items: 4,
                 dots: false,
-                center: true
+                center: true,
+                responsive:{
+                    0:{
+                        items: 2
+                    },
+                    767:{
+                        items: 4
+                    }
+                }
     });
 
     $('.owl-carousel.review-slider').owlCarousel({
@@ -22,7 +37,7 @@ $(document).ready(function(){
                 autoplay:true, //Автозапуск слайдера
                 smartSpeed:3500, //Время движения слайда
                 autoplayTimeout:6000, //Время смены слайда
-                mouseDrag: true,
+                mouseDrag: false,
                 touchDrag: true,
                 margin: 20,
                 dots: false, 
@@ -30,18 +45,66 @@ $(document).ready(function(){
                     0:{
                         items: 1
                     },
-                    767:{
+                    600:{
                         items: 2,
-                        margin: 120
+                        margin: 100
                     },
                     991:{
                         items: 3
                     }
                 }
     });
+
     $(".toggle-mnu").click(function() {
-        $(this).toggleClass("on");
+        $(".toggle-mnu").toggleClass("on");
         $(".nav").toggleClass("active");
         return false;
+    });
+
+    $("body").on("click","a.nav__link", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    var objToStick = $(".nav");
+    var topOfObjToStick = 35;
+    $(window).scroll(function () {
+        var windowScroll = $(window).scrollTop();
+        if (windowScroll > topOfObjToStick) {
+            $(objToStick).addClass("topWindow");
+        } else {
+            $(objToStick).removeClass("topWindow");
+        };
+    });
+
+    $(".info-map__check").on("click", function (e) {
+        var arrCity = $(".info-maps").find(".info-maps__text .info-maps__city");
+        for (var i = 0; i < arrCity.length; i++) {
+            arrCity.eq(i).animate({
+                    fontSize: "0.875em"
+                }, 300);
+            arrCity.eq(i).css('color', 'rgba(255, 255, 255, 0.7)');
+            if ($(this).text() == arrCity.eq(i).text())
+            {
+                arrCity.eq(i).animate({
+                    fontSize: "1.25em"
+                }, 1000);
+                arrCity.eq(i).css('color', '#fff');
+            } 
+        }
+        $("info-maps").find("info-maps__text").text();
+    });
+
+    $(".contact-form__input").on("focus", function (e) {
+        var arrName = $(".contact-form__form").find("label");
+        for (var a = 0; a < arrName.length; a++){
+            if($(this).attr('name') == arrName.eq(a).attr('for')){
+                arrName.eq(a).animate({
+                    opacity: 0
+                }, 600);
+            }
+        }
     });
 });
